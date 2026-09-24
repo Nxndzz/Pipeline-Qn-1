2e11b21f9d96e86e4b8e2afa549a44af1d3d2222
unknown <Administrator@SJT19SITE018.VITUNIVERSITY.LOCAL>
2026-09-24 10:37:27 +0530

    First Commit

---
+++ test_app.py
@@ -0,0 +1,8 @@
+from app import square,is_even
+def test_square():
+    assert square(5) == 25
+    assert square(4) == 16
+
+def test_is_even():
+    assert is_even(4)==True
+    assert is_even(5)==False
